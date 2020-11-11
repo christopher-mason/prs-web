a78df13843f3c68a350046fc459b2d136bb668da
Chris Mason <m1achris43@gmail.com>
2020-11-11 17:53:51 -0500

    Added Request

--- src/main/java/com/prs/web/RequestController.java
+++ src/main/java/com/prs/web/RequestController.java
@@ -1,8 +1,12 @@
 package com.prs.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.prs.business.Request;
 import com.prs.db.RequestRepo;
 
 @CrossOrigin
@@ -12,6 +16,44 @@ public class RequestController {
 	
 	@Autowired
 	public RequestRepo requestRepo; 
+	
+	// Get All
+		@GetMapping("/")
+		public List<Request> getAll() {
+			return requestRepo.findAll();
+		}
+		
+		// Get by ID
+		@GetMapping("/{id}")
+		public Optional<Request> getById(@PathVariable int id) {
+			return requestRepo.findById(id);
+		}
+		
+		// Add Request
+		@PostMapping("/")
+		public Request addRequest(@RequestBody Request r) {
+			r = requestRepo.save(r);
+			return r;
+		}
+		
+		// Update a Request
+		@PutMapping("/")
+		public Request updateRequest(@RequestBody Request r) {
+			r = requestRepo.save(r);
+			return r;
+		}
+		
+		// Delete a Product
+		@DeleteMapping("/{id}")
+		public Request deleteRequest(@PathVariable int id) {
+			Optional<Request> r = requestRepo.findById(id);
+			if (r.isPresent()) {
+				requestRepo.deleteById(id);
+			} else {
+				System.out.println("Error - Product not found for id: " + id);
+			}
+			return r.get();
+		}
 	
 	
 
